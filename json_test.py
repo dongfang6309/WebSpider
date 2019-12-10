51107f138c00373d830f68a6f0ae0695d6a31d13
dongfang6309 <jiahaozhang@zju.edu.cn>
2019-12-11 00:51:37 +0800

    modify the variable s:str

--- json_test.py
+++ json_test.py
@@ -8,7 +8,7 @@ print(d, '--', type(d))
 j2 = json.dumps(d, ensure_ascii=False)
 print(j2, '--', type(j2))
 
-s = '哈哈哈'
+s = '哈哈哈哈'
 s1 = s.encode('utf-8')
 s2 = s1.decode('utf-8')
 print(s1, '---', type(s1))
