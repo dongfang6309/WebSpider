900992ea35ea9877bedba645707a8331c3f31739
dongfang6309 <jiahaozhang@zju.edu.cn>
2019-12-15 16:46:41 +0800

    move some functions from the file: tieba_spider.py to urllib_func.py

--- tieba_spider.py
+++ tieba_spider.py
@@ -14,40 +14,9 @@ def get_url(url, req_str, page):
     return url_tem
 
 
-def get_header_info(path_header, path_userAgent, path_proxy):
-    userAgent, userAgentLib = urf.user_agent(path_userAgent)
-    userProxy, userProxyLib = urf.user_proxy(path_proxy)
-    file_header = open(path_header, 'r')
-    WebHeader: str = file_header.read()
-    web_header = json.loads(WebHeader)
-    file_header.close()
-    # web_header['User-Agent'] = userAgent
-
-    proxy_handler = urllib.request.ProxyHandler(userProxy)
-    http_handler = urllib.request.HTTPHandler()
-    url_opener = urllib.request.build_opener(http_handler)
-    return web_header, url_opener
-
-
-def get_html_code(url, web_header, url_opener):
-    urllib.request.install_opener(url_opener)
-
-    # 进行get请求
-    req = urllib.request.Request(url, headers=web_header)
-    response = urllib.request.urlopen(req)
-    html_code = response.read()
-    return html_code
-
-
-def write_html_code(html_code, path_phpFile, page):
-    phpFile = open(path_phpFile, 'wb')
-    phpFile.write(html_code)
-    phpFile.close()
-
-
 def get_spider(url, req_str, page_ini, page_end,
                path_header, path_userAgent, path_proxy, path_phpFile):
-    web_header, url_opener = get_header_info(path_header, path_userAgent, path_proxy)
+    web_header, url_opener = urf.get_header_info(path_header, path_userAgent, path_proxy)
     print(web_header['User-Agent'])
     req_str_pin = pinyin(req_str, style=Style.NORMAL)
     req_piny = ''
@@ -55,10 +24,10 @@ def get_spider(url, req_str, page_ini, page_end,
         req_piny = req_piny + each[0]
     for page in range(page_ini, page_end + 1):
         url_tem = get_url(url, req_str, page)
-        html_code_temp = get_html_code(url_tem, web_header, url_opener)
+        html_code_temp = urf.get_html_code(url_tem, web_header, url_opener)
         path_phpFile_tem = path_phpFile + r'\%s_tieba_page%d.php' % (req_piny, page)
         print('正在下载%s贴吧第%d页' % (req_str, page))
-        write_html_code(html_code_temp, path_phpFile_tem, page)
+        urf.write_html_code(html_code_temp, path_phpFile_tem)
 
 
 if __name__ == "__main__":
--- urllib_func.py
+++ urllib_func.py
@@ -24,3 +24,34 @@ def user_proxy(path_proxy):
     userProxyLib = json.loads(userProxyLib)
     userProxy = random.choice(userProxyLib)
     return userProxy, userProxyLib
+
+
+def get_header_info(path_header, path_userAgent, path_proxy):
+    userAgent, userAgentLib = user_agent(path_userAgent)
+    userProxy, userProxyLib = user_proxy(path_proxy)
+    file_header = open(path_header, 'r')
+    WebHeader: str = file_header.read()
+    web_header = json.loads(WebHeader)
+    file_header.close()
+    # web_header['User-Agent'] = userAgent
+
+    proxy_handler = urllib.request.ProxyHandler(userProxy)
+    http_handler = urllib.request.HTTPHandler()
+    url_opener = urllib.request.build_opener(http_handler)
+    return web_header, url_opener
+
+
+def get_html_code(url, web_header, url_opener):
+    urllib.request.install_opener(url_opener)
+
+    # 进行get请求
+    req = urllib.request.Request(url, headers=web_header)
+    response = urllib.request.urlopen(req)
+    html_code = response.read()
+    return html_code
+
+
+def write_html_code(html_code, path_phpFile):
+    phpFile = open(path_phpFile, 'wb')
+    phpFile.write(html_code)
+    phpFile.close()
